1e871f298210432a8cb4a324c49358962d2adb47
Alexander Y <startyourday60@gmail.com>
2025-09-20 13:27:32 +0000

    feat: Docker

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+COMMAND=/usr/local/bin/i2pd
+# To make ports exposeable
+# Note: $DATA_DIR is defined in /etc/profile
+
+if [ "$1" = "--help" ]; then
+    set -- $COMMAND --help
+else
+    ln -s /i2pd_certificates "$DATA_DIR"/certificates
+    set -- $COMMAND $DEFAULT_ARGS $@
+fi
+
+"$@" &
+
+sleep 5
+
+socat TCP-LISTEN:9313,fork TCP:outproxys:9313
